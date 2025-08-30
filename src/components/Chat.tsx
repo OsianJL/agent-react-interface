18bcbc456fc88ccae744d6944e33decb78c9d441
OsianJL <osianjorge@gmail.com>
2025-08-30 17:50:07 +0100

    .env added

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -19,12 +19,12 @@ export default function Chat() {
     setInput('');
 
     try {
-      const res = await fetch('http://localhost:8000/chat', {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/chat`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: input }),
       });
-    
+
       const data = await res.json();
 
       const content =
@@ -47,7 +47,7 @@ export default function Chat() {
         },
       ]);
     }
-    
+
   };
 
   useEffect(() => {
